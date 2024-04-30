3e5a71921f91cef752ae3df8ff279e4abbe8d3a3
olga <olga.kurzepa@simfactor.pl>
2024-04-30 11:21:51 +0200

    From Post To Delete Tests

--- src/test/java/CitizenController_Tests.java
+++ src/test/java/CitizenController_Tests.java
@@ -4,24 +4,27 @@ import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+@Test(groups = "FromPostToDeleteTests")
 public class CitizenController_Tests {
-    String newName = "nowe";
-    int id = 50;
+    String newName = "noweimie";
+    int id = 0;
 
-    @Test
+    @Test(priority = 1)
     public void testPOST() {
         RequestSpecification request = RestAssured.given();
         request.header("Content-Type", "application/json");
-        request.body("{\"id\": 50, \"name\": \"Olg\", \"lastName\": \"Olg\", \"pesel\": \"99101011111\", \"dateOfBirth\": \"10-10-1990\"}");
+        request.body("{\"id\": "+id+", \"name\": \"Temporary\", \"lastName\": \"Olg\", \"pesel\": \"99101011111\", \"dateOfBirth\": \"10-10-1990\"}");
         Response response = (Response) request.post(TestBase.citizenControllerUrl, new Object[0]);
         Assert.assertEquals(response.statusCode(), TestBase.statusCodeCreated);
-        String bla = response.path("name");
-        Assert.assertEquals(bla, "Olg");
+        String actualName = response.path("name");
+        id = response.path("id");
+        Assert.assertEquals(actualName, "Temporary");
+        System.out.println(id);
     }
 
-    @Test
+    @Test(priority = 2)
     public void testPUT() {
-
+        Assert.assertNotEquals(id, 0);
         RequestSpecification request = RestAssured.given();
         request.header("Content-Type", "application/json");
         request.body("{\"id\": "+id+", \"name\": \"" + newName + "\", \"lastName\": \"Olga\", \"pesel\": \"99101011111\", \"dateOfBirth\": \"10-10-1990\"}");
@@ -33,11 +36,17 @@ public class CitizenController_Tests {
         Assert.assertEquals(actualId,id);
     }
 
-    @Test
+    @Test(priority = 3)
     public void testGET() {
-        Response response = RestAssured.get(TestBase.citizenControllerUrl + "/50");
+        Assert.assertNotEquals(id, 0);
+        Response response = RestAssured.get(TestBase.citizenControllerUrl + "/"+id);
+        Assert.assertEquals(response.statusCode(), TestBase.statusCodeSuccessOK);
+        Assert.assertEquals(response.path("name"),newName);
+    }@Test(priority = 4)
+    public void testDELETE() {
+        Assert.assertNotEquals(id, 0);
+        Response response = RestAssured.delete(TestBase.citizenControllerUrl + "/"+id);
         Assert.assertEquals(response.statusCode(), TestBase.statusCodeSuccessOK);
     }
-
 
 }
